0261add91f13798ce9df4f086fc16e92939b7d43
Chinzilla2000 <113079280+Chinzilla2000@users.noreply.github.com>
2023-03-04 13:57:09 -0500

    Kulvectus, Fallen Omen... Start!
    You Mom

---
+++ NPCs/Bosses/Kulvectus/Kulvectus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ModLoader;
+
+namespace Annihilation.NPCs.Bosses.Kulvectus
+{
+    public class Kulvectus : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Kulvectus, Fallen Omen");
+            Main.npcFrameCount[NPC.type] = 8;
+        }
+        public override void SetDefaults()
+        {
+            NPC.width = 48;
+            NPC.height = 68;
+            NPC.damage = 12;
+            NPC.defense = 8;
+            NPC.lifeMax = 1500;
+            NPC.rarity = 4; // Enemies Rarity = 1, Miniboss Rarity = 2, Rare Enemies Rarity = 3, Bosses And NPCs Rarity = 4, Late Game Bosses Rarity = 5.
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime,
+                new FlavorTextBestiaryInfoElement("The Hungre!")
+            });
+        }
+    }
+}
