f95305d520e2f5aba99229f2b57078b47358e7e2
Delta Bec Noir <coolcraft938@gmail.com>
2023-03-04 21:44:34 -0800

    fixed the kulvectus sprites also counts as a boss

--- NPCs/Bosses/Kulvectus/Kulvectus.cs
+++ NPCs/Bosses/Kulvectus/Kulvectus.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.ID;
 using Terraria.GameContent.Bestiary;
 using Terraria.ModLoader;
 
@@ -18,12 +19,20 @@ namespace Annihilation.NPCs.Bosses.Kulvectus
         }
         public override void SetDefaults()
         {
-            NPC.width = 48;
-            NPC.height = 68;
+            NPC.width = 96;
+            NPC.height = 136;
             NPC.damage = 12;
             NPC.defense = 8;
             NPC.lifeMax = 1500;
+            NPC.noGravity = true;
+            NPC.boss = true;
+            NPC.lavaImmune = true;
+            NPC.noTileCollide = true;
+            NPC.knockBackResist = 0f;
+            NPC.HitSound = SoundID.NPCHit8;
+            NPC.DeathSound = SoundID.NPCDeath10;
             NPC.rarity = 4; // Enemies Rarity = 1, Miniboss Rarity = 2, Rare Enemies Rarity = 3, Bosses And NPCs Rarity = 4, Late Game Bosses Rarity = 5.
+            if (!Main.dedServ) Music = MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Kulvectus");
         }
         public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
         {
