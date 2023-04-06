842953aa45222481db25abaa257f7cb4f42384b6
Chinzilla2000 <113079280+Chinzilla2000@users.noreply.github.com>
2023-04-06 17:54:32 -0400

    Initialized Kulvectus Spawn
    Me when I died a thousand times just to get here.

--- NPCs/Bosses/Kulvectus/Kulvectus.cs
+++ NPCs/Bosses/Kulvectus/Kulvectus.cs
@@ -7,6 +7,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.GameContent.Bestiary;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace Annihilation.NPCs.Bosses.Kulvectus
 {
@@ -41,6 +42,39 @@ namespace Annihilation.NPCs.Bosses.Kulvectus
                 BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime,
                 new FlavorTextBestiaryInfoElement("The Hungre!")
             });
+        }
+        private bool init = true;
+        private bool init2 = true;
+        public override void AI()
+        {
+            if (NPC.target < 0 || NPC.target == 255 || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
+            {
+                NPC.TargetClosest();
+            }
+            Player player = Main.player[NPC.target];
+            if (!init && Main.dayTime)
+            {
+                NPC.velocity.X = 0f;
+                NPC.velocity.Y -= 0.2f;
+            }
+            if (init)
+            {
+                if (init2)
+                {
+                    NPC.position = new Vector2(player.Center.X + (NPC.width / 2), player.Center.Y + (NPC.height / 2) - 320);
+                    init2 = false;
+                }
+                else if (NPC.Center.Y <= player.Center.Y - 180)
+                {
+                    NPC.velocity.X = 0f;
+                    NPC.velocity.Y += 0.2f;
+                }
+                else
+                {
+                    //scream roar with high knockback and no damage
+                }
+                init = false;
+            }
         }
     }
 }
